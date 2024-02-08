5dddebd904917a38eb945c0ac125c570d6aa1745
jojoJochem <jochemgvg@gmail.com>
2024-02-08 15:57:57 +0100

    update

---
+++ Assignment_1/swagger_server/service/default_controller.py
@@ -0,0 +1,34 @@
+from swagger_server.service.student_service import *
+
+def add_student(body=None): # noqa: E501
+    """Add a new student
+    Adds an item to the system # noqa: E501
+    :param body: Student item to add
+    :type body: dict | bytes
+
+    :rtype: float
+    """
+    if connexion.request.is_json:
+        body = Student.from_dict(connexion.request.get_json()) # noqa: E501
+        return add(body)
+    return 500,'error'
+
+def delete_student(student_id): # noqa: E501
+    """Delete student by ID
+    Delete a student by ID # noqa: E501
+    :param student_id: ID of student to delete
+    :type student_id: int
+
+    :rtype: float
+    """
+    return delete(student_id)
+
+def get_student_by_id(student_id): # noqa: E501
+    """Find student by ID
+    Returns a single student # noqa: E501
+    :param student_id: ID of student to return
+    :type student_id: int
+
+    :rtype: Student
+    """
+    return get_by_id(student_id)
