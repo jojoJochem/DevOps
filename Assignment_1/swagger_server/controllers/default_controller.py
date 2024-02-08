5541fc542f7d9dc03f26abeb4382e4b27d20d3ee
jojoJochem <jochemgvg@gmail.com>
2024-02-08 16:40:32 +0100

    postman

--- Assignment_1/swagger_server/controllers/default_controller.py
+++ Assignment_1/swagger_server/controllers/default_controller.py
@@ -3,44 +3,38 @@ import six
 
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
+from swagger_server.service.student_service import *
 
 
-def add_student(body=None):  # noqa: E501
+def add_student(body=None): # noqa: E501
     """Add a new student
-
     Adds an item to the system # noqa: E501
-
     :param body: Student item to add
     :type body: dict | bytes
 
     :rtype: float
     """
     if connexion.request.is_json:
-        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        body = Student.from_dict(connexion.request.get_json()) # noqa: E501
+        return add(body)
+    return 500,'error'
 
+def delete_student(student_id): # noqa: E501
+    """Delete student by ID
+    Delete a student by ID # noqa: E501
+    :param student_id: ID of student to delete
+    :type student_id: int
 
-def delete_student(student_id):  # noqa: E501
-    """deletes a student
-
-    delete a single student   # noqa: E501
-
-    :param student_id: the uid
-    :type student_id: float
-
-    :rtype: None
+    :rtype: float
     """
-    return 'do some magic!'
+    return delete(student_id)
 
-
-def get_student_by_id(student_id):  # noqa: E501
-    """gets student
-
-    Returns a single student  # noqa: E501
-
-    :param student_id: the uid
-    :type student_id: float
+def get_student_by_id(student_id): # noqa: E501
+    """Find student by ID
+    Returns a single student # noqa: E501
+    :param student_id: ID of student to return
+    :type student_id: int
 
     :rtype: Student
     """
-    return 'do some magic!'
+    return get_by_id(student_id)
